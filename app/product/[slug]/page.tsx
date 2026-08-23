aca11f0c95147d88d5dd8124a0627e252152e681
CodingProjectDev <yteasycode@gmail.com>
2026-08-23 17:30:33 -0500

    Import useRouter in page.tsx

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import {
   ExternalLink,
