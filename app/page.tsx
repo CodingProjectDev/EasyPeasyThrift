3b25077d95064a80aacbe768fc5601d0de0682cb
CodingProjectDev <yteasycode@gmail.com>
2026-08-26 19:39:25 -0500

    Fix Catagory display

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function HomePage() {
       }
     }
 
-    return Array.from(byCategory.entries()).slice(0, 3);
+    return Array.from(byCategory.entries());
   }, [products]);
 
   return (
